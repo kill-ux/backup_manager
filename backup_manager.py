561983929fbb667418398751e68003c83452d150
killux <mustaphaboutoubdev@gmail.com>
2026-01-29 16:59:34 +0100

    some changes

---
+++ backup_manager.py
@@ -0,0 +1,23 @@
+import os, sys
+
+
+    
+
+    
+def main():
+    with open("backup_schedules.txt", os.R_OK) as fd:
+        if len(sys.argv) != 2:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
