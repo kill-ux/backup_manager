50771e524274f1abd91bd1343b89c2074ee79a69
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-01-30 17:32:55 +0100

    fixed backups on same minute twice

--- service/event_loop.py
+++ service/event_loop.py
@@ -2,18 +2,23 @@ import time
 from .get_schedules import get_schedules
 from .extract_info import check_and_extract_info
 from .make_backup import make_backup
+from datetime import datetime
+
 
 
 def run():
+    last_processed_minute = ""
     while True:
-        for schedule in get_schedules():
-            info = check_and_extract_info(schedule)
-            if info is None:
-                continue
-            path, backup_name = info
+        now = datetime.now()
+        current_time_str = now.strftime("%H:%M")
+        if current_time_str != last_processed_minute: 
+            for schedule in get_schedules():
+                info = check_and_extract_info(schedule)
+                if info is None:
+                    continue
+                path, backup_name = info
+                make_backup(path, backup_name)
+        last_processed_minute = current_time_str
             
-            make_backup(path, backup_name)
-            
-            pass
         
-        time.sleep(60)
+        time.sleep(45)
