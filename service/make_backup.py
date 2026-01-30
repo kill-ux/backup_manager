84a0f6dbefa62a7f47f8a0188efd2a0fc3bc94e5
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-01-30 17:43:59 +0100

    save

--- service/make_backup.py
+++ service/make_backup.py
@@ -10,15 +10,12 @@ def make_backup(path: str, backup_name: str):
         if not backup_name.endswith(".tar"):
             backup_name = f"{backup_name}.tar"
             
-        # destination = os.path.join(BACKUPS_DIR, full_filename)
         destination = BACKUPS_DIR + "/" + backup_name
         
-        
         with tarfile.open(destination, "w") as tar:
-            tar.add(path)  #  arcname=os.path.basename(path)
-        
+            tar.add(path) 
 
-        print(format(f"Backup done for {path} in backups/{backup_name}"))
+        print(format(f"INFO: Backup done for {path} in backups/{backup_name}"))
             
     except Exception as e:
         print(format(f"Error: Failed to create backup for {path}: {e}"))
