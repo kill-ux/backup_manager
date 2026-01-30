95284d79f1df378858350b7abfce2492177e2d43
kill-ux <mustaphaboutoubdev@gmail.com>
2026-01-30 10:19:19 +0100

    j

--- backup_manager.py
+++ backup_manager.py
@@ -1,23 +1,12 @@
 import os, sys, re
-from manager.create import create
 from utils.create_dir import create_dir
+from manager.command import run
 
 
 
 def main():
     create_dir("logs")
-
-    with open("./logs/backup_manager.log", "w") as fd:
-        match sys.argv[1:]:
-            case ["start"]:
-                print("Starting...")
-            case ["stop"]:
-                print("Stopping...")
-            case ["create",schedule]:
-                create(schedule)
-            case _:
-                print("Invalid arguments")
-                fd.write(f"Unknown command: {sys.argv[1:]}\n")
+    run(sys.argv[1:])
 
 
 
