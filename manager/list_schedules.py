dfa687eb6d7073da614fbe2a03ebd6c89810c46b
kill-ux <mustaphaboutoubdev@gmail.com>
2026-01-30 11:48:13 +0100

    fix list

--- manager/list_schedules.py
+++ manager/list_schedules.py
@@ -6,7 +6,7 @@ def list_schedules():
     try:
         with open(SCHEDULES_FILE) as fd:
             for index, schedule in enumerate(fd):
-                print(f"{index}: {schedule}")
+                print(f"{index}: {schedule}", end="")
             log_message("TRACE: Show schedules list")
     except Exception as e:
         log_message(f"Error: Unexpected error while list a schedules: {e}")
